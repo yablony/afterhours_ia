050f9fcd170d2139a21bbcf447d10a72d0426328
Benreefgit config --global core.editor code <ben@outrun.io>
2023-06-01 14:04:11 +0930

    added initilize page

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -1,1 +1,18 @@
-const state = {}
+const state = {
+  quotes: []
+}
+
+fetch('/api/quotes')
+  .then(res => res.json())
+  .then(quotes => {
+    state.quotes = quotes
+    renderQuote()
+  })
+
+fetch('/api/sessions')
+  .then(res => res.json())
+  .then(data => {
+    if (data.result === 'successful') {
+      state.loggedInUser = data.email
+    }
+  })
