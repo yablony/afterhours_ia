b6871342e15d9e9c1d05a149e96540a76c838390
yablony <yablonskaya.yuliya@gmail.com>
2023-06-01 15:51:50 +0930

    Made changes to the quote_controller and quotes in models

--- controllers/quotes_controller.js
+++ controllers/quotes_controller.js
@@ -2,3 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const Quote = require('../models/quotes');
+
+router.get('/', (req, res) => {
+    Quote
+        .finAll()
+        .then(quotes => res.json(quotes))
+});
+
+module.exports = router;
