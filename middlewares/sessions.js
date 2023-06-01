3a369ff0c167d4f45e46979f1742dfb20fadfe62
yablony <yablonskaya.yuliya@gmail.com>
2023-06-01 10:28:20 +0930

    Changed the middleware files, server, and added files in the js, css, controllers directories

---
+++ middlewares/sessions.js
@@ -0,0 +1,10 @@
+const session = require('express-session')
+
+// we are using express-session to create sessions, to make a cookie
+module.exports = session({
+    key: 'user_sid',
+    secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24} // 1000miliseconds(which is 1s * 60s(becomes one minute) * 60mins(becomes one hour) * 24h(becomes one day))
+})
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 
+const logger = require('./middlewares/logger')
+const quotesController = require('./controllers/quotes_controller')
+const usersController = require('./controllers/users_controller')
+const sessionsContoller = require('./controllers/sessions_controller')
+const sessions = require('./middlewares/sessions')
+
 const app = express();
 const port = 3000;
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+app.use(logger) 
+app.use(express.static('client')) 
+app.use(express.json()) // middleware to parse JSON body in a POST, PUT, or DELETE request, and it assigns the data to req.body
+app.use(sessions) 
+
+// this works like url prefix in flask // when we pass on two arguments, it's smart enough to understand that the first argument is a route and the second one is a middleware route function
+app.use('/api/quotes', quotesController) 
+app.use('/api/users', usersController)
+app.use('/api/sessions', sessionsContoller)
