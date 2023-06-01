3da69b785918141b7a2931bcee86ebca0c3a4602
howaboutdale <1daletromp@gmail.com>
2023-06-01 14:32:04 +0930

    login.js complete

--- client/js/components/login.js
+++ client/js/components/login.js
@@ -16,3 +16,34 @@ function renderLogin() {
     </section>
     `
 }
+
+function logIn(event) {
+    event.preventDefault()
+    const form = event.target
+
+    // to convert the form ata into an object
+    const data = Object.fromEntries(new FormData(form))
+
+    fetch('/api/sessions', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+
+        .then(res => res.json())
+        .then(res => {
+            if (res.error) {
+                renderLogin()
+                renderError(res.error)
+            } else {
+                state.loggedInUser = res.email
+                renderQuoteList()
+            }
+        })
+}
+
+function renderError(errorMessage) {
+    document.querySelector('#page').innerHTML =
+        `<h2 class="error">${errorMessage}</h2>` +
+        document.querySelector('#page').innerHTML
+}
