f93a60f7c306802007dbfc00b44f26939343f792
howaboutdale <1daletromp@gmail.com>
2023-06-01 09:53:00 +0930

    Updated Schema.SQL

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE afterhours_ia;
+\c afterhours_ia
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    name TEXT,
+    email TEXT,
+    password_digest TEXT
+);
+
+CREATE TABLE quotes(
+    id SERIAL PRIMARY KEY,
+    user_id TEXT,
+    name TEXT,
+    quote TEXT,
+);
+
+INSERT INTO quotes(user_id, name, quote)
+VALUES
+    ('1', 'Kasun', "Don't do your work on the main branch"),
+    ('1', 'Bree', 'Have you tried asking your question with efficiency?'),
+    ('1', 'Neil', 'Console log it!'),
+    ('1', 'Jordan', 'Wanna go to a breakout room?');
+
+
